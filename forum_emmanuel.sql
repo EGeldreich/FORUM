f278066a8b74cd9222fb4a2855717059a577cc32
Emmanuel Geldreich <egeldreich@gmail.com>
2024-12-20 09:32:43 +0100

    Updated DB and Topic Model so that the first message is part of the Topic entity

--- forum_emmanuel.sql
+++ forum_emmanuel.sql
@@ -35,6 +35,52 @@ INSERT INTO `category` (`id_category`, `categoryName`) VALUES
 	(4, 'Food Conservation'),
 	(2, 'Gardening');
 
+-- Listage de la structure de table forum_emmanuel. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `mail` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'user',
+  `registrationDate` date DEFAULT (curdate()),
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `nickname` (`nickname`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Listage des données de la table forum_emmanuel.user : ~0 rows (environ)
+INSERT INTO `user` (`id_user`, `nickname`, `password`, `mail`, `role`, `registrationDate`) VALUES
+	(1, 'john_doe', 'password123', 'john_doe@example.com', 'user', '2024-12-18'),
+	(2, 'jane_smith', 'password123', 'jane_smith@example.com', 'user', '2024-12-18'),
+	(3, 'michael_brown', 'password123', 'michael_brown@example.com', 'user', '2024-12-18'),
+	(4, 'lisa_white', 'password123', 'lisa_white@example.com', 'user', '2024-12-18'),
+	(5, 'susan_black', 'password123', 'susan_black@example.com', 'user', '2024-12-18'),
+	(6, 'david_green', 'password123', 'david_green@example.com', 'user', '2024-12-18');
+
+  -- Listage de la structure de table forum_emmanuel. topic
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id_topic` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `content` text COLLATE utf8mb4_bin NOT NULL,
+  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `closed` tinyint NOT NULL DEFAULT '0',
+  `category_id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_topic`) USING BTREE,
+  KEY `category_id` (`category_id`) USING BTREE,
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `FK_topic_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
+  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Listage des données de la table forum_emmanuel.topic : ~0 rows (environ)
+INSERT INTO `topic` (`id_topic`, `title`, `content`, `creationDate`, `closed`, `category_id`, `user_id`) VALUES
+	(1, 'Living the Farm Life', 'The first message of the topic', '2024-12-18 16:32:14', 0, 1, 1),
+	(2, 'Tips for a Beautiful Garden', 'The first message of the topic', '2024-12-18 16:32:14', 0, 2, 2),
+	(3, 'Raising Healthy Chickens', 'The first message of the topic', '2024-12-18 16:32:14', 0, 3, 3),
+	(4, 'Methods of Food Preservation', 'The first message of the topic', '2024-12-18 16:32:14', 0, 4, 4),
+	(5, 'Building Your Own Shed', 'The first message of the topic', '2024-12-18 16:32:14', 0, 5, 5);
+
 -- Listage de la structure de table forum_emmanuel. post
 CREATE TABLE IF NOT EXISTS `post` (
   `id_post` int NOT NULL AUTO_INCREMENT,
@@ -61,51 +107,6 @@ INSERT INTO `post` (`id_post`, `postDate`, `content`, `topic_id`, `user_id`) VAL
 	(8, '2024-12-18 16:32:23', 'Drying and freezing are also excellent preservation techniques.', 4, 2),
 	(9, '2024-12-18 16:32:23', 'A good shed starts with a solid foundation.', 5, 3),
 	(10, '2024-12-18 16:32:23', 'Make sure to use quality materials for your DIY build.', 5, 4);
-
--- Listage de la structure de table forum_emmanuel. topic
-CREATE TABLE IF NOT EXISTS `topic` (
-  `id_topic` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) COLLATE utf8mb4_bin NOT NULL,
-  `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `closed` tinyint NOT NULL DEFAULT '0',
-  `category_id` int NOT NULL,
-  `user_id` int DEFAULT NULL,
-  PRIMARY KEY (`id_topic`) USING BTREE,
-  KEY `category_id` (`category_id`) USING BTREE,
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `FK_topic_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
-  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
--- Listage des données de la table forum_emmanuel.topic : ~0 rows (environ)
-INSERT INTO `topic` (`id_topic`, `title`, `creationDate`, `closed`, `category_id`, `user_id`) VALUES
-	(1, 'Living the Farm Life', '2024-12-18 16:32:14', 0, 1, 1),
-	(2, 'Tips for a Beautiful Garden', '2024-12-18 16:32:14', 0, 2, 2),
-	(3, 'Raising Healthy Chickens', '2024-12-18 16:32:14', 0, 3, 3),
-	(4, 'Methods of Food Preservation', '2024-12-18 16:32:14', 0, 4, 4),
-	(5, 'Building Your Own Shed', '2024-12-18 16:32:14', 0, 5, 5);
-
--- Listage de la structure de table forum_emmanuel. user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
-  `nickname` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `mail` varchar(150) COLLATE utf8mb4_bin NOT NULL,
-  `role` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'user',
-  `registrationDate` date DEFAULT (curdate()),
-  PRIMARY KEY (`id_user`),
-  UNIQUE KEY `nickname` (`nickname`),
-  UNIQUE KEY `mail` (`mail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
--- Listage des données de la table forum_emmanuel.user : ~0 rows (environ)
-INSERT INTO `user` (`id_user`, `nickname`, `password`, `mail`, `role`, `registrationDate`) VALUES
-	(1, 'john_doe', 'password123', 'john_doe@example.com', 'user', '2024-12-18'),
-	(2, 'jane_smith', 'password123', 'jane_smith@example.com', 'user', '2024-12-18'),
-	(3, 'michael_brown', 'password123', 'michael_brown@example.com', 'user', '2024-12-18'),
-	(4, 'lisa_white', 'password123', 'lisa_white@example.com', 'user', '2024-12-18'),
-	(5, 'susan_black', 'password123', 'susan_black@example.com', 'user', '2024-12-18'),
-	(6, 'david_green', 'password123', 'david_green@example.com', 'user', '2024-12-18');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
