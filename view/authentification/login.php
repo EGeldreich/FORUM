e1f2355461f95e9a798457bcd34c6ef2d3beae83
Emmanuel G <stdgeld@gmail.com>
2025-01-06 11:20:20 +0100

    Added CSS and modified HTML for login and register pages

--- view/authentification/login.php
+++ view/authentification/login.php
@@ -2,12 +2,12 @@
     $mail = $result["data"]['mail'] ?? NULL;
 ?>
 <div class="form login-form column">
-    <h1>Login</h1>
+    <h1 class="outfit">Login</h1>
 
-    <form class="" method="post" action="index.php?ctrl=security&action=login">
+    <form class="column" method="post" action="index.php?ctrl=security&action=login">
         <!-- Change url of link in layout ? -->
         <div class="input-and-label column">
-            <label for="mail">Mail</label>
+            <label for="mail" class="sat20">Mail</label>
             <input type="email" name="mail" id="mail" placeholder="Email" 
             <?php if($mail) {?> 
                 value="<?= $mail ?>"
@@ -16,12 +16,12 @@
         </div>
 
         <div class="input-and-label column">
-            <label for="password">Password</label>
+            <label for="password" class="sat20">Password</label>
             <input type="password" name="password" id="password" placeholder="Password" required>
-            <a class="form-link__mini" href="#">Forgot your password ?</a>
+            <a class="form-link form-link__mini" href="#">Forgot your password ?</a>
         </div>
 
-        <input type="submit" value="Login" name="login">
+        <input type="submit" value="Login" name="login" class="big-btn outfit">
     </form>
 
     <p>No account yet ? <a class="form-link" href="index.php?ctrl=security&action=registerPage">Register</a></p>
--- view/authentification/register.php
+++ view/authentification/register.php
@@ -1,29 +1,29 @@
 <div class="form register-form column">
-    <h1>Register</h1>
+    <h1 class="outfit">Register</h1>
 
-    <form class="" method="post" action="index.php?ctrl=security&action=register">
+    <form class="column" method="post" action="index.php?ctrl=security&action=register">
         <!-- Change url of link in layout ? -->
         <div class="input-and-label column">
-            <label for="mail">Mail</label>
+            <label class="sat20" for="mail">Mail</label>
             <input type="email" name="mail" id="mail" placeholder="Email" required>
         </div>
 
         <div class="input-and-label column">
-            <label for="pseudo">Pseudo</label>
+            <label class="sat20" for="pseudo">Pseudo</label>
             <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required>
         </div>
 
         <div class="input-and-label column">
-            <label for="password1">Password</label>
+            <label class="sat20" for="password1">Password</label>
             <input type="password" name="pass1" id="password1" placeholder="Password" required>
         </div>
 
         <div class="input-and-label column">
-            <label for="password2">Confirm your password</label>
+            <label class="sat20" for="password2">Confirm your password</label>
             <input type="password" name="pass2" id="password2" placeholder="Confirm your password" required>
         </div>
 
-        <input type="submit" value="Create account" name="register">
+        <input class="big-btn outfit" type="submit" value="Create account" name="register">
     </form>
 
     <p>Already registered ? <a class="form-link" href="index.php?ctrl=security&action=loginPage">Login</a></p>
