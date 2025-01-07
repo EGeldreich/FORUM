340ef2105b17dc6d12495894d5648a5912776374
Emmanuel GELDREICh <egeldreich@gmail.com>
2025-01-07 16:14:16 +0100

    Topic now has the correct content display (still missing reply, lock and delete btns)

--- view/forum/topic.php
+++ view/forum/topic.php
@@ -1,39 +1,102 @@
 <?php
     $topic = $result["data"]['topic'];
     $posts = $result["data"]['posts'];
+
+    use Service\Functions;
+    $functions = new Functions();
 ?>
 
-<br>
-<br>
-<h1>SPECIFIC TOPIC</h1>
-<br>
-<br>
-<table>
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Category</th>
-            <th>User</th>
-            <th>Nb Topics</th>
-            <th>Nb Posts</th>
-            <th>Date</th>
-            <th>Content</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><?= $topic->getTitle() ?></td>
-            <td><?= $topic->getCategory() ?></td>
-            <td><?= $topic->getUser() ?></td>
-            <td><?= $topic->getTotalTopics()?></td>
-            <td><?= $topic->getTotalPosts()?></td>
-            <td><?= $topic->getCreationDate()?></td>
-            <td><?= $topic->getContent()?></td>
-        </tr>
-    </tbody>
-</table>
-<br>
-<br>
+<div class="fixed-section fixed-section__topic shadow">
+    <div class="scrollable-content scrollable-content__topic column">
+
+        <div class="topic_title_cat">
+            <h3 class="outfit">
+                <?= $topic->getTitle() ?>
+            </h3>
+            <div class="info_cat">
+                <p class="info_cat_btn <?= substr($topic->getCategory(), 0, 3)?>">
+                    <?= $topic->getCategory()?>
+                </p>
+            </div>
+        </div>
+
+        <div class="topic-post row">
+            <p class="topic-post_content"><?= $topic->getContent()?></p>
+            <div class="poster-info column">
+                <div class="poster-info_pp_stats row">
+                    <div class="user_pp">
+                        <p class="h3">
+                            <?= substr($topic->getUser(), 0, 1)?>
+                        </p>
+                    </div>
+                    <div class="stats column">
+                        <p><?= $topic->getTotalTopics()?> topics</p>
+                        <p><?= $topic->getTotalPosts()?> posts</p>
+                    </div>
+                </div>
+                <div class="poster-info_pseudo_time">
+                    <p class="pseudo outfit">
+                        <?= $topic->getUser()?>
+                    </p>
+                    <p class="post_time sat18">
+                        <?= $functions->timeElapsed($topic->getCreationDate()); ?>
+                    </p>
+                </div>
+                <div class="post-count">
+                    <p class="sat20">
+                        0
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <?php foreach($posts as $post){
+            if($post->getId()) { // Check if the post is valid (because there is at minimum an empty post in $posts?>
+                <div class="topic-post row">
+                    <div class="poster-info column">
+                        <div class="poster-info_pp_stats row">
+                            <div class="user_pp">
+                                <p class="h3">
+                                    <?= substr($post->getUser(), 0, 1)?>
+                                </p>
+                            </div>
+                            <div class="stats column">
+                                <p><?= $post->getTotalTopics()?> topics</p>
+                                <p><?= $post->getTotalPosts()?> posts</p>
+                            </div>
+                        </div>
+                        <div class="poster-info_pseudo_time">
+                            <p class="pseudo outfit">
+                                <?= $post->getUser()?>
+                            </p>
+                            <p class="post_time sat18">
+                                <?= $functions->timeElapsed($post->getPostDate()); ?>
+                            </p>
+                        </div>
+                        <div class="post-count">
+                            <p class="sat20">
+                            <?= $post->getResponseNumber()?>
+                            </p>
+                        </div>
+                    </div>
+                    <p class="topic-post_content"><?= $post->getPostContent()?></p>
+                </div>
+        <?php }
+        } ?>
+    </div>
+</div>
+
+        
+
+<?php if($topic->getClosed() == 0 && App\Session::getUser()){ ?>
+    <div class="form post-form column">
+        <form  method="post" action='index.php?ctrl=post&action=newPost&id=<?= $topic->getId() ?>'>
+                <textarea name="content" id="content" placeholder="Write your reply here" required></textarea>
+            <input type="submit" value="Post" name="newPost">
+        </form>
+    </div>
+<?php } ?>
+
 <?php
 if($topic->getUser() == App\Session::getUser() || App\Session::getUser()->hasRole("ROLE_ADMIN")){ 
     if($topic->getClosed() == 0) { ?>
@@ -50,42 +113,3 @@ if(App\Session::getUser()->hasRole("ROLE_ADMIN")) { ?>
     </a>
 <?php }
 ?>
-
-<br>
-<br>
-
-<h4>POSTS</h4>
-<table>
-    <thead>
-        <tr>
-            <th>User</th>
-            <th>Nb Topics</th>
-            <th>Nb Posts</th>
-            <th>Content</th>
-            <th>responseNumber</th>
-            <th>Date</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($posts as $post){ ?>
-            <tr>
-                <td><?= $post->getUser()?></td>
-                <td><?= $post->getTotalTopics()?></td>
-                <td><?= $post->getTotalPosts()?></td>
-                <td><?= $post->getPostContent()?></td>
-                <td><?= $post->getResponseNumber()?></td>
-                <td><?= $post->getPostDate()?></td>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
-<br>
-<br>
-<?php if($topic->getClosed() == 0 && App\Session::getUser()){ ?>
-    <div class="form post-form column">
-        <form  method="post" action='index.php?ctrl=post&action=newPost&id=<?= $topic->getId() ?>'>
-                <textarea name="content" id="content" placeholder="Write your reply here" required></textarea>
-            <input type="submit" value="Post" name="newPost">
-        </form>
-    </div>
-<?php } ?>
